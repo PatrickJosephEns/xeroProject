eaecef92955cfbcc7200e3c70146677e53b51868
melda2 <50220285+melda2@users.noreply.github.com>
2021-06-10 13:22:34 +1200

    fixing for deployment

--- xero-netstandard-oauth2-starter-dotnet-core-master/XeroNetStandardApp/Startup.cs
+++ xero-netstandard-oauth2-starter-dotnet-core-master/XeroNetStandardApp/Startup.cs
@@ -41,7 +41,7 @@ namespace XeroNetStandardApp
              options.AddDefaultPolicy(
                               builder =>
                                {
-                                   builder.WithOrigins("http://52.63.164.208/");
+                                   builder.WithOrigins("http://localhost:5001");
                                });
           });
     }

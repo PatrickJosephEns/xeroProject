492d9913b65993e1ceb56441b22aeda26b632f1d
PatrickJosephEns <patens6@gmail.com>
2021-06-21 14:28:38 +1200

    done

--- react-frontend/src/components/pages/home.js
+++ react-frontend/src/components/pages/home.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect, location, Link} from "react";
+import '../../App.css';
  
 function Home() {
 
   return (   
           <div>
-            <p>Xero is a New Zealand domiciled public technology company, listed on the Australian Securities Exchange. Xero is a cloud-based accounting software platform for small and medium-sized businesses.</p>
+            <img className = 'homeImg' src='imgs/XeroIllustration.jpg'></img>
+            <p>Xero is a New Zealand domiciled public technology company,</p>
+            <p> listed on the Australian Securities Exchange. Xero is a cloud-based </p> 
+            <p>accounting software platform for small and medium-sized businesses.</p>
           </div>
   );
 }
